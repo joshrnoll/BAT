f44c00765b0062b3d8f3272ad33495ede9c14656
joshrnoll <103843711+joshrnoll@users.noreply.github.com>
2023-09-25 19:16:22 -0500

    Initial Commit

---
+++ BAT/BAT.psm1
@@ -0,0 +1,114 @@
+﻿####################################################################
+#################################################################### 
+#####        
+#####        Name: Bastogne Automations Tool (BAT) 
+#####        Author: Joshua R. Noll
+#####        Version: 1.2
+#####        Usage: help .\BAT
+#####
+####################################################################
+####################################################################
+
+###################################################################
+##################       Main function for BAT     ################
+###################################################################
+
+function BAT
+{
+
+<#
+
+.SYNOPSIS
+This module is an automation tool for helpdesk operations on an Army network.
+
+.DESCRIPTION
+This module is dependent upon an ATCTS report being present in the working directory with the following fields:
+
+    - EDIPI
+    - HQ Alignment Subunit
+    - Name
+    - Rank
+    - Profile Verified
+    - Date SAAR/DD2875 Signed
+    - Date Awareness Training Completed
+    - Date Most Recent Army IT UA Doc Signed
+    - Enterprise Email Address
+
+Alternatively to being present in the working directory, you can specify the path to the report with the -Path parameter.
+
+.LINK
+https://github.com/joshrnoll/BAT
+https://atcts.army.mil
+
+#>
+
+    ######## BAT parameters #########  
+    param
+    (
+        #One or more EDIPIs (DoD ID numbers) of a user or users, separated by commas
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
+        [string[]]$EDIPIs,
+
+        #The full or relative path to the ATCTS report. By default, this value is .\report_export.csv
+        [Parameter(Mandatory=$false)]
+        [System.IO.FileInfo]$Path = ".\report_export.csv",
+
+        #Specifies to check for an account in Active Directory
+        [Parameter(Mandatory=$false)]
+        [switch]$CheckAD,
+
+        #Specifies to display ATCTS compliance information
+        [Parameter(Mandatory=$false)]
+        [switch]$CheckATCTS,
+        
+        #Specifies to enable the users
+        [Parameter(Mandatory=$false)]
+        [switch]$Enable,
+
+        #Specifies whether or not to log output to a .txt file in the working directory. Files are written to the working directory.
+        [Parameter(Mandatory=$false)]
+        [switch]$Log
+    )
+    
+    if (!($CheckAD) -and !($CheckATCTS) -and !($Enable))
+    {
+        Write-Warning "You have not provided any parameters. Run help BAT -full for help."
+    }
+    
+    if ($CheckATCTS)
+    {
+        if ($Log)
+        {
+            Get-ATCTS -Path $Path -EDIPIs $EDIPIs -Log
+        }
+        else
+        {
+            Get-ATCTS -Path $Path -EDIPIs $EDIPIs
+        }
+    }
+    
+    if ($CheckAD)
+    {
+        if($Log)
+        {
+           Find-ADUser -EDIPIs $EDIPIs -Log 
+        }
+        else
+        {
+           Find-ADUser -EDIPIs $EDIPIs
+        }
+    }
+    
+    if ($Enable)
+    {
+        if ($Log)
+        {
+            Enable-ADUser -Path $Path -EDIPIs $EDIPIs -Log
+        }
+
+        else
+        {
+            Enable-ADUser -Path $Path -EDIPIs $EDIPIs
+        }
+    } 
+}
--- README.md
+++ README.md
@@ -1,2 +1,64 @@
+<img src="./Bastogne Logo.png" width="300" height="300">
+
 # BAT
 Bastogne Automation Tool
+
+# Description
+BAT is a PowerShell module used for streamlining helpdesk operations for an Army network. This tool helps to automate the process of checking user's compliance in ATCTS alongside account creation/modification in Active Directory.
+
+# Features
+<ul>
+  <li> Check ATCTS compliance </li>
+  <li> Check if user has an AD account (including visitors accounts) </li>
+  <li> Enable users in AD </li>
+  <li> Log output for later reference </li>
+</ul>
+
+# Installation
+<ol>
+  <li> Click the 'code' dropdown menu and download the .zip file. </li>
+  <li> Extract the contents and find the BAT folder. </li>
+  <li> The BAT folder should have the following contents/structure: </li> <br>
+  <ul> 
+    <li> BAT-Library </li>
+    <ul>
+      <li> BAT-Library.psm1 </li>
+    </ul>
+    <li> BAT.psd1 </li>
+    <li> BAT.psm1 </li> <br>
+  </ul>
+  <li> Copy the BAT folder and all of its contents to <code>C:\Program Files\WindowsPowerShell\Modules</code> </li>
+  <li> Open a PowerShell window and run: <code>Import-Module BAT</code> </li>
+  <li> For usage instructions run: <code>Get-Help BAT</code> </li>
+</ol>
+
+# Usage
+<ul>
+  <li> Basic syntax: </li> 
+  
+`bat [EDIPIs] [Options]`
+
+  <li> Options: </li>
+  
+  <ul>
+    <li> <strong>CheckATCTS:</strong> This will check the user(s) ATCTS report and provide feedback on delinquent items. </li>
+    <li> <strong>CheckAD:</strong> This will report whether or not the user is found in Active Directory. It will also identify if the user has a DoD visitor account on Ft. Campbell. </li>
+    <li> <strong>Enable:</strong> This will enable the user, only if their ATCTS report is clean. <em>(requires admin credentials)</em></li>
+    <li> <strong>Log:</strong> This will log the output to a .txt file in the working directory. </li>
+    <li> <strong>Path:</strong> This will specify the path to your ATCTS report (.csv file) -- default value is <strong>.\report_export</strong> if not specified</li>
+  </ul> <br>
+
+  <li> Simple Examples: </li>
+
+`bat 1234567890 -CheckATCTS` -- This will check a single user's ATCTS report <br>
+`bat 1234567890 -CheckAD` -- This will check if the user exists in Active Directory <br>
+`bat 1234567890 -Enable` -- This will enable the user, but only if their ATCTS report is clean <br>
+  
+  <li> Dynamic Examples: </li>
+
+`bat 1234567890,3216549870,4560123987 -CheckATCTS` -- Check multiple user's ATCTS <br>
+`bat 1234567890,3216549870,4560123987 -CheckATCTS -CheckAD` -- Check multiple user's ATCTS reports and check for accounts in Active Directory <br>
+`bat 1234567890,3216549870,4560123987 -CheckATCTS -Log` -- Check multiple user's ATCTS and log the output <br>
+
+</ul>
+
